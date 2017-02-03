bfbb2de8a71174ceed7a0e2694323e90dcfe77eb
Will Strimling <w@ml.to>
2017-02-03 01:18:55 -0800

    refactor test

--- test/compile/mark/label.test.ts
+++ test/compile/mark/label.test.ts
@@ -3,52 +3,56 @@
 import {assert} from 'chai';
 import {parseLayerModel} from '../../util';
 import {label} from '../../../src/compile/mark/label';
+import {Mark} from '../../../src/mark';
 import {ANCHOR, OFFSET} from '../../../src/channel';
 import {ExtendedUnitSpec} from '../../../src/spec';
 
 describe('Mark: Label', function() {
   it('should return correct marktype', function() {
-    assert.equal(label.markType(), 'label');
+    assert.equal(label.vgMark, 'label');
   });
 
-  describe('simple bar', function() {
+  describe('simple bar', function () {
     const model = parseLayerModel({
-      "description": "A simple bar chart with embedded data.",
-      "layers": [
+      description: 'A simple bar chart with embedded data.',
+      layer: [
         {
-          "name": "myBar",
-          "mark": "bar",
-          "data": {
-            "values": [
-              {"a": "A","b": 28},
-              {"a": "B","b": 55},
-              {"a": "C","b": 43},
-              {"a": "D","b": 91},
-              {"a": "E","b": 81},
-              {"a": "F","b": 53},
-              {"a": "G","b": 2},
-              {"a": "H","b": 87},
-              {"a": "I","b": 52}
+          name: 'myBar',
+          mark: 'bar',
+          data: {
+            values: [
+              {a: 'A',b: 28},
+              {a: 'B',b: 55},
+              {a: 'C',b: 43},
+              {a: 'D',b: 91},
+              {a: 'E',b: 81},
+              {a: 'F',b: 53},
+              {a: 'G',b: 2},
+              {a: 'H',b: 87},
+              {a: 'I',b: 52}
             ]
           },
-          "encoding": {
-            "x": {"field": "a","type": "ordinal"},
-            "y": {"field": "b","type": "quantitative"}
+          encoding: {
+            x: {field: 'a',type: 'ordinal'},
+            y: {field: 'b',type: 'quantitative'}
           }
         },
         {
-          "mark": "label",
-          "data": {"source": "myBar"},
-          "encoding": {
-            "color": {"value": "auto"},
-            "text": {"field": "b"},
-            "anchor": {"value": "auto"},
-            "offset": {"value": "auto"}
+          mark: 'label',
+          data: { ref: 'myBar' },
+          encoding: {
+            color: {value: 'auto'},
+            text: {field: 'b'},
+            anchor: {value: 'auto'},
+            offset: {value: 'auto'}
           }
         }
       ]
     });
-    const props = label.encodeEntry(model);
+
+    // const props = label.encodeEntry(model);
+    const labelMarks = model.children().filter((child) => typeof child.mark === Mark.LABEL);
+    const encodedLabels = labelMarks.map((labelMark) => label.encodeEntry(labelMark));
 
     it('should have correct ANCHOR', function() {
       assert.deepEqual(true, true);
